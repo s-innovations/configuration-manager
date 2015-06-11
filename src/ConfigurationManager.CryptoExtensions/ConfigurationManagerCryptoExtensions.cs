5f0a0bf5e0b5ffcfb5a1aaad809e581385fc42fc
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-06-11 02:03:06 +0200

    Added a few extensions for decrypting setting values

---
+++ src/ConfigurationManager.CryptoExtensions/ConfigurationManagerCryptoExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.ConfigurationManager.CryptoExtensions
+{
+    public static class ConfigurationManagerCryptoExtensions
+    {
+      
+        public static string GetAndDecryptSetting(this ConfigurationManager manager,string key,string CertKey)
+        {
+            var certData = Convert.FromBase64String(manager.GetSetting<string>(CertKey));
+            var cert = new X509Certificate2(certData,new SecureString(),X509KeyStorageFlags.Exportable);
+            return cert.Decrypt(manager.GetSetting<string>(key),true);
+
+        }
+    }
+}
---
+++ src/ConfigurationManager.CryptoExtensions/EncryptionHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.ConfigurationManager.CryptoExtensions
+{
+    public static class EncryptionHelper
+    {
+        public static string Encrypt(this X509Certificate2 certificate,string settingValue, bool fOAEP)
+        {
+
+
+            if (settingValue == null)
+            {
+                throw new ArgumentNullException("settingValue");
+            }
+            if (certificate == null)
+            {
+                throw new ArgumentNullException("certificate");
+            }
+
+            byte[] plainData = Encoding.UTF8.GetBytes(settingValue);
+
+            using (RSACryptoServiceProvider provider = new RSACryptoServiceProvider())
+            {
+                // Note that we use the public key to encrypt
+                provider.FromXmlString(GetPublicKey(certificate));
+                return Convert.ToBase64String(provider.Encrypt(plainData, fOAEP));
+            }
+        }
+        public static string GetPublicKey(X509Certificate2 certificate)
+        {
+            if (certificate == null)
+            {
+                throw new ArgumentNullException("certificate");
+            }
+
+            return certificate.PublicKey.Key.ToXmlString(false);
+        }
+
+        public static string Decrypt(this X509Certificate2 certificate, string encryptedBase64String, bool fOAEP)
+                               
+        {
+
+            if (encryptedBase64String == null)
+            {
+                throw new ArgumentNullException("encryptedBase64String");
+            }
+            if (certificate == null)
+            {
+                throw new ArgumentNullException("certificate");
+            }
+            var encryptedData = Convert.FromBase64String(encryptedBase64String);
+
+            using (RSACryptoServiceProvider provider = new RSACryptoServiceProvider())
+            {
+                // Note that we use the private key to decrypt
+                provider.FromXmlString(GetXmlKeyPair(certificate));
+
+                return Encoding.UTF8.GetString(provider.Decrypt(encryptedData, fOAEP));
+            }
+        }
+        public static string GetXmlKeyPair(X509Certificate2 certificate)
+        {
+            if (certificate == null)
+            {
+                throw new ArgumentNullException("certificate");
+            }
+
+            if (!certificate.HasPrivateKey)
+            {
+                throw new ArgumentException("certificate does not have a private key");
+            }
+            else
+            {
+                return certificate.PrivateKey.ToXmlString(true);
+            }
+        }
+    }
+}
