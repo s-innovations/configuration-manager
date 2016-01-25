454e6c2b87e989f8884b543b2c40874c4de7b388
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-01-25 07:17:39 +0100

    added securestring support for secret

--- src/AzureKeyVaultSettingsProvider/AzureKeyVaultDefaults.cs
+++ src/AzureKeyVaultSettingsProvider/AzureKeyVaultDefaults.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security;
+using System.Runtime.InteropServices;
 
 namespace SInnovations.ConfigurationManager.Providers
 {
@@ -28,6 +30,38 @@ namespace SInnovations.ConfigurationManager.Providers
                 secretName, secretVersion).Trim('/'),
                 (str) => JsonConvert.DeserializeObject<Secret>(str));
         }
+        public static void RegisterAzureKeyVaultSecretSecureString(this ConfigurationManager config, string secretName, string secretVersion = null, string name = null)
+        {
+            config.RegisterSetting(name ?? secretName, () => string.Format("{0}/secrets/{1}/{2}",
+                  config.GetSetting<string>(AzureKeyVaultDefaults.DefaultKeyVaultUriKey).Trim('/'),
+                  secretName, secretVersion).Trim('/'),
+                (str) => convertToSecureString( JsonConvert.DeserializeObject<Secret>(str).Value));
+        }
+        public static SecureString convertToSecureString(string strPassword)
+        {
+            var secureStr = new SecureString();
+            if (strPassword.Length > 0)
+            {
+                foreach (var c in strPassword.ToCharArray()) secureStr.AppendChar(c);
+            }
+            return secureStr;
+        }
+        public static string ConvertToUnsecureString(this SecureString securePassword)
+        {
+            if (securePassword == null)
+                throw new ArgumentNullException("securePassword");
+
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
 
         public static Secret GetAzureKeyVaultSecret(this ConfigurationManager config, string name)
         {
@@ -43,5 +77,18 @@ namespace SInnovations.ConfigurationManager.Providers
             return value;
         }
        
+        public static SecureString GetAureKeyVaultSecretSecureString(this ConfigurationManager config, string name)
+        {
+            SecureString value;
+            if (!config.TryGetSetting<SecureString>(name, out value))
+            {
+                config.RegisterAzureKeyVaultSecret(name);
+                config.TryGetSetting<SecureString>(name, out value);
+            }
+            if (value == null)
+                throw new Exception(string.Format("Could not get secret {0}", name));
+
+            return value;
+        }
     }
 }
