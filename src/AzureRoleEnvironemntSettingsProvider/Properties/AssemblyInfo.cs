aaf460e12d744e7a289da60623292acfd6498e80
Poul Kjeldager Sørensen <pks@s-innovations.net>
2016-03-31 21:47:45 +0200

    fixed decription

--- src/AzureRoleEnvironemntSettingsProvider/Properties/AssemblyInfo.cs
+++ src/AzureRoleEnvironemntSettingsProvider/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("S-Innovations.ConfigurationManager.AzureRoleEnvironemntSettingsProvider")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Azure Cloud Service Configuration Settings Provider")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("S-Innovations v/Poul Kjeldager Sørensen")]
 [assembly: AssemblyProduct("S-Innovations.ConfigurationManager.AzureRoleEnvironemntSettingsProvider")]
